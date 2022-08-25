50458a725263912be7b1dc427a098bc7cdb2fe3a
lupeMorales <morales.lupegmail.com>
2022-08-25 10:33:02 +0200

    update

--- src/components/SolutionLetters.js
+++ src/components/SolutionLetters.js
@@ -1,4 +1,5 @@
 import "../styles/Letters.scss";
+import Form from "./Form";
 const SolutionLetters = (props) => {
   return (
     <section>
@@ -10,23 +11,7 @@ const SolutionLetters = (props) => {
         <h2 className="title">Letras falladas:</h2>
         <ul className="letters">{props.renderErrorLetters}</ul>
       </div>
-      <form className="form" onSubmit={props.handleSubmit}>
-        <label className="title" htmlFor="last-letter">
-          Escribe una letra:
-        </label>
-        <input
-          autoFocus
-          autoComplete="off"
-          className="form__input"
-          maxLength="1"
-          type="text"
-          name="last-letter"
-          id="last-letter"
-          value={props.lastLetter}
-          onKeyDown={props.handleKeyDown}
-          onChange={props.handleChange}
-        />
-      </form>
+      <Form />
     </section>
   );
 };
